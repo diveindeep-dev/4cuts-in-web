5f83a33626e0c84fd0aca4b5c40b0b6dfff97915
hakyung <diveindeep.dev@gmail.com>
2023-04-18 20:33:23 +0900

    Add webcam and multiple capture with react-webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import Webcam from 'react-webcam';
+import styled from 'styled-components';
+
+const Preview = styled.img`
+  width: 180px;
+`;
 
 function App() {
+  const [imgSrcs, setImgSrcs] = useState<string[]>([]);
+  const webcamRef = useRef<any>(null);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrcs((prev) => [...prev, imageSrc]);
+  }, [webcamRef, setImgSrcs]);
+
   return (
     <div className="App">
       <h1>4CUTS in Web</h1>
+      <Webcam ref={webcamRef} screenshotFormat="image/jpeg" audio={false} />
+      <button onClick={capture}>📸</button>
+      {imgSrcs.map((imgSrc: string, i: number) => {
+        return <Preview key={i} src={imgSrc} alt={`take${i}`} />;
+      })}
     </div>
   );
 }
